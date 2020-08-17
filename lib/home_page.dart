728a448ce8c6716a5ff755bc9d0ad3ab0f24856f
0xhacksy <hansy.schmitt.dev@gmail.com>
2020-08-16 21:05:34 -0500

    Update rooms and users

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  String name = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +17,9 @@ class HomePage extends StatelessWidget {
           child: Column(
             children: [
               TextField(
+                onChanged: (String newName) {
+                  this.name = newName;
+                },
                 decoration: InputDecoration(hintText: "Player name"),
               ),
               Expanded(
@@ -27,8 +33,40 @@ class HomePage extends StatelessWidget {
   }
 
   FirebaseAuth _auth = FirebaseAuth.instance;
+  Firestore _store = Firestore.instance;
   void createRoom() async {
     AuthResult result = await _auth.signInAnonymously();
     String uid = result.user.uid;
+    String roomName = 'tempa';
+    //Creates or updates user id
+    _store.collection('users').document(uid).setData(({
+          'username': name,
+        }));
+    //Create or updates rooms
+    _store
+        .collection('rooms')
+        .where('creator', isEqualTo: uid)
+        .where('name', isEqualTo: roomName)
+        .snapshots()
+        .first
+        .then((QuerySnapshot value) {
+      if (value.documents.length > 0) {
+        String documentId = value.documents.first.documentID;
+        _store.collection('rooms').document(documentId).setData({
+          'active': true,
+          'creator': uid,
+          'name': roomName,
+          'player': '',
+        });
+      } else {
+        _store.collection('rooms').document().setData({
+          'active': true,
+          'creator': uid,
+          'name': roomName,
+          'player': '',
+        });
+      }
+    });
+    /* ;*/
   }
 }
